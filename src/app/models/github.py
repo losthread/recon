ccd13d2b66dfa8f9170d195da704a6824f18c888
f8tum <parthnaik.office@gmail.com>
2026-07-09 16:54:15 +0530

    feat: create github user model

---
+++ src/app/models/github.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+@dataclass
+class GithubProfile:
+  username: str
+  id: str
+  name: str
+  profile_url: str
+  avatar_url: str
+  bio: str
+  followers: int
+  folowing: int
+  location: str
+  company: str
+  blog: str
+  email: str
+  twitter_username: str
+  created_at: str
