25dcf700a69a28431f9fed4ddaf21ae8a867777f
f8tum <parthnaik.office@gmail.com>
2026-07-09 14:54:52 +0530

    fix: handle null safety

--- src/app/fetchers/reddit.py
+++ src/app/fetchers/reddit.py
@@ -16,19 +16,20 @@ def fetch_reddit_user_details(username: str) -> RedditProfile:
   response.raise_for_status()
   
   # convert json to dict
-  data = response.json()["data"]
+  data = response.json().get("data", [])
   if not data:
     return None
   
   # returns a list of dicts
   user = data[0]
+  meta = user.get("_meta", {})
 
   return RedditProfile(
-    username=user["author"],
-    id=user["id"],
-    karma=user["_meta"]["total_karma"],
-    comments=user["_meta"]["num_comments"],
-    posts=user["_meta"]["num_posts"],
+    username=user.get("author"),
+    id=user.get("id"),
+    karma=meta.get("total_karma", 0),
+    comments=meta.get("num_comments", 0),
+    posts=meta.get("num_posts", 0),
   )
 
 # fetch user's top 100 posts
@@ -51,11 +52,11 @@ def fetch_reddit_user_posts(username: str, limit: int = 100) -> list[RedditPost]
   posts = list()
   for post in data:
     posts.append(RedditPost(
-      id=post["id"],
-      title=post["title"],
-      body=post["selftext"],
-      subreddit=post["subreddit"],
-      created_utc=post["created_utc"],
+      id=post.get("id"),
+      title=post.get("title"),
+      body=post.get("selftext"),
+      subreddit=post.get("subreddit"),
+      created_utc=post.get("created_utc"),
     ))
   
   return posts
@@ -80,10 +81,10 @@ def fetch_reddit_user_comments(username: str, limit: int = 100) -> list[RedditCo
   comments = list()
   for comment in data:
     comments.append(RedditComment(
-      id=comment['id'],
-      body=comment['body'],
-      subreddit=comment['subreddit'],
-      created_utc=comment['created_utc'],
+      id=comment.get('id'),
+      body=comment.get('body'),
+      subreddit=comment.get('subreddit'),
+      created_utc=comment.get('created_utc'),
     ))
 
   return comments
